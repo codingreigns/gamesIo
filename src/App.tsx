261341a64cf010c3435e1ccff38c0e6bae8c21db
astroboy <codingreigns@gmail.com>
2024-11-21 22:21:11 +0300

    Build a responsive layout(kinda)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const availb = window.screen.availWidth;
+  const [screenWidth, setScreenWidth] = useState(availb);
+
+  useEffect(() => {
+    setScreenWidth(availb);
+  }, [availb]);
+  console.log(availb);
   return (
     <>
-      <Button colorScheme={"light"}>Hello world</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav""main"`,
+          md: `"nav nav" "aside main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area={"nav"} bg={"red"}>
+          nav
+        </GridItem>
+        {screenWidth > 768 ? <GridItem bg={"blue"}>aside</GridItem> : ""}
+
+        <GridItem area={"main"} bg={"green"}>
+          main
+        </GridItem>
+      </Grid>
     </>
   );
 }
