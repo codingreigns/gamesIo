a408c02ffae782658ca05d7c21b5d003e9f5b313
astroboy <codingreigns@gmail.com>
2024-11-21 22:41:10 +0300

    Implement dark mode toggle

--- src/components/mycomponents/Navbar.tsx
+++ src/components/mycomponents/Navbar.tsx
@@ -1,10 +1,12 @@
 import { HStack, Image } from "@chakra-ui/react";
 
 import logo from "@/assets/Logo/logo.webp";
+import { ColorModeButton } from "../ui/color-mode";
 const Navbar = () => {
   return (
-    <HStack>
+    <HStack justifyContent={"space-between"} padding={"10px"}>
       <Image src={logo} boxSize={20} />
+      <ColorModeButton />
     </HStack>
   );
 };
