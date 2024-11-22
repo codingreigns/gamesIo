d551e92ee14e63d37af16f19b14764cca1aa26e1
astroboy <codingreigns@gmail.com>
2024-11-22 12:40:47 +0300

    refactor:removed duplication using claude sonet

---
+++ src/components/mycomponents/GameCardSkeleton.tsx
@@ -0,0 +1,23 @@
+import { SkeletonText } from "@/components/ui/skeleton";
+import { Card, HStack, Skeleton } from "@chakra-ui/react";
+
+const GameCardSkeleton = () => {
+  return (
+    <Card.Root maxW="sm" width={"300px"} overflow="hidden">
+      {/* <Image src={cropImageUrl(game.background_image)} /> */}
+      <Skeleton height={"200px"} />
+      <Card.Body gap="2">
+        {/* <Card.Title>{game.name}</Card.Title> */}
+        <SkeletonText noOfLines={1} />
+        <Card.Description as={"div"}>
+          <HStack justifyContent={"space-between"}>
+            <SkeletonText noOfLines={1} width={"75px"} />
+            <SkeletonText marginLeft={"90px"} width={"30px"} noOfLines={1} />
+          </HStack>
+        </Card.Description>
+      </Card.Body>
+    </Card.Root>
+  );
+};
+
+export default GameCardSkeleton;
--- src/components/mycomponents/PlatformIconList.tsx
+++ src/components/mycomponents/PlatformIconList.tsx
@@ -11,54 +11,33 @@ import {
 } from "react-icons/fa";
 import { MdPhoneIphone } from "react-icons/md";
 import { SiNintendo } from "react-icons/si";
-// import { IconType } from "react-icons";
+import { IconType } from "react-icons";
 
 interface Props {
   platforms: Platform[];
 }
+
+const iconMap: { [key: string]: IconType } = {
+  pc: FaWindows,
+  playstation: FaPlaystation,
+  xbox: FaXbox,
+  nintendo: SiNintendo,
+  mac: FaApple,
+  linux: FaLinux,
+  ios: MdPhoneIphone,
+  web: BsGlobe,
+  android: FaAndroid,
+};
+
 const PlatformIconList = ({ platforms }: Props) => {
-  // const iconMap: { [key: string]: IconType } = {
-  //   pc: FaWindows,
-  //   playstation: FaPlaystation,
-  //   xbox: FaXbox,
-  //   nintendo: SiNintendo,
-  //   mac: FaApple,
-  //   linux: FaLinux,
-  //   ios: MdPhoneIphone,
-  //   web: BsGlobe,
-  //   android: FaAndroid,
-  // };
   return (
     <HStack>
-      {platforms.map((platform) =>
-        platform.slug === "pc" ? (
-          <FaWindows key={platform.id} />
-        ) : platform.slug === "playstation" ? (
-          <FaPlaystation key={platform.id} />
-        ) : platform.slug === "xbox" ? (
-          <FaXbox key={platform.id} />
-        ) : platform.slug === "nintendo" ? (
-          <SiNintendo key={platform.id} />
-        ) : platform.slug === "mac" ? (
-          <FaApple key={platform.id} />
-        ) : platform.slug === "linux" ? (
-          <FaLinux key={platform.id} />
-        ) : platform.slug === "ios" ? (
-          <MdPhoneIphone key={platform.id} />
-        ) : platform.slug === "web" ? (
-          <BsGlobe key={platform.id} />
-        ) : platform.slug === "android" ? (
-          <FaAndroid key={platform.id} />
-        ) : (
-          ""
-        )
-      )}
+      {platforms.map((platform) => {
+        const IconComponent = iconMap[platform.slug];
+        return IconComponent ? <IconComponent key={platform.id} /> : null;
+      })}
     </HStack>
   );
 };
-
-// const CustomIcon = ()=>{
-//   return
-// }
 
 export default PlatformIconList;
