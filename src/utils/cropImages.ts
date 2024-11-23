4197d1b168b9cb9d7694c7abf1b2115c90292727
astroboy <codingreigns@gmail.com>
2024-11-23 12:14:25 +0300

    Add a placeholder image

--- src/utils/cropImages.ts
+++ src/utils/cropImages.ts
@@ -1,5 +1,7 @@
+import placeholder from "@/assets/Image Placeholder/no-image-placeholder-6f3882e0.webp";
+
 const cropImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return placeholder;
   const index = url.indexOf("media/") + "media/".length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
 };
